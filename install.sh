091bf1c5dee1742b692551057b2d249a8bf6817c
Trittium <38106610+Trittium@users.noreply.github.com>
2018-05-21 08:06:52 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -155,7 +155,7 @@ BLOCKCOUNT=0
 until su -c "trittium-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" $USER; do
   for (( i=0; i<${#CHARS}; i++ )); do
     sleep 2
-    BLOCKCOUNT='su -c "trittium-cli getblockcount 2>/dev/null" $USER'
+    BLOCKCOUNT='su -l -c "trittium-cli getblockcount" $USER'
 	echo -en "${CHARS:$i:1}" "$BLOCKCOUNT" "\r"
 #	echo -en "$BLOCKCOUNT" "\r"
   done
